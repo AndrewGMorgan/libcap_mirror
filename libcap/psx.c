551bbc17d64237da0555a817fd4552bd706dd987
Andrew G. Morgan <morgan@kernel.org>
2020-03-07 09:56:59 -0800

    Replace pthread_yeild() with sched_yield().
    The latter is POSIX standard whereas the former is apparently not.
    
    Patch contributed by Adam Schaefers, addressing these bugs:
    
    https://bugzilla.kernel.org/show_bug.cgi?id=206741
    https://github.com/kisslinux/community/issues/495
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- libcap/psx.c
+++ libcap/psx.c
@@ -14,6 +14,7 @@
 
 #include <errno.h>
 #include <pthread.h>
+#include <sched.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -533,7 +534,7 @@ long int __psx_syscall(long int syscall_nr, ...) {
 	if (!waiting) {
 	    break;
 	}
-	pthread_yield();
+	sched_yield();
     }
 
     errno = restore_errno;
