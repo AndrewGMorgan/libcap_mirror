80f7b5383b168912e67aa7d6480bb2bb6a66e880
Andrew G. Morgan <morgan@kernel.org>
2020-07-25 17:21:09 -0700

    Test errno behavior for psx syscalls.
    Theoretical concern that Go might, at some point, switch threads
    inside the psx.Syscall*() functions and yield an incorrect errno
    return value (errno is held in thread local storage). To avoid
    this, lock OS threads. Also, add a test that the right things will
    happen if C.errno is non-zero before a call to psx.Syscall[36]().
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- psx/psx.go
+++ psx/psx.go
@@ -1,9 +1,14 @@
 // Package psx provides support for system calls that are run
 // simultanously on all pthreads.
 //
+// This property can be used to work around a lack of native Go
+// support for such a feature. Something that is the subject of:
+//
+//   https://github.com/golang/go/issues/1435
+//
 // The package works via CGo wrappers for system call functions that
-// call the C libpsx functions of these names. This ensures that the
-// system calls execute symultaneously on all the pthreads of the Go
+// call the C [lib]psx functions of these names. This ensures that the
+// system calls execute simultaneously on all the pthreads of the Go
 // (and CGo) combined runtime. Since Go's runtime freely migrates code
 // execution between pthreads, support of this type is required for
 // any successful attempt to fully drop or modify user privilege of a
@@ -23,7 +28,7 @@
 //    https://go-review.googlesource.com/c/go/+/236139/
 //
 // As of the time of writing, that is all release tags prior to
-// go1.15beta1 .
+// go1.15rc1 .
 //
 // The workaround is to build with the following CGO_LDFLAGS_ALLOW
 // in effect:
@@ -34,6 +39,7 @@
 package psx // import "kernel.org/pub/linux/libs/security/libcap/psx"
 
 import (
+	"runtime"
 	"syscall"
 )
 
@@ -43,27 +49,54 @@ import (
 // #include <errno.h>
 // #include <sys/psx_syscall.h>
 //
-// long __errno_too() { return errno ; }
+// long __errno_too(long set_errno) {
+//     long v = errno;
+//     if (set_errno >= 0) {
+//       errno = set_errno;
+//     }
+//     return v;
+// }
 import "C"
 
+// setErrno returns the current C.errno value and, if v >= 0, sets the
+// CGo errno for a random pthread to value v. If you want some
+// consistency, this needs to be called from runtime.LockOSThread()
+// code. This function is only defined for testing purposes. The psx.c
+// code should properly handle the case that a non-zero errno is saved
+// and restored independently of what these Syscall[36]() functions
+// observe.
+func setErrno(v int) int {
+	return int(C.__errno_too(C.long(v)))
+}
+
 // Syscall3 performs a 3 argument syscall using the libpsx C function
-// psx_syscall3().
+// psx_syscall3(). Syscall3 differs from syscall.[Raw]Syscall()
+// insofar as it is simultaneously executed on every pthread of the
+// combined Go and CGo runtimes.
 func Syscall3(syscallnr, arg1, arg2, arg3 uintptr) (uintptr, uintptr, syscall.Errno) {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
 	v := C.psx_syscall3(C.long(syscallnr), C.long(arg1), C.long(arg2), C.long(arg3))
 	var errno syscall.Errno
 	if v < 0 {
-		errno = syscall.Errno(C.__errno_too())
+		errno = syscall.Errno(C.__errno_too(-1))
 	}
 	return uintptr(v), uintptr(v), errno
 }
 
 // Syscall6 performs a 6 argument syscall using the libpsx C function
-// psx_syscall6()
+// psx_syscall6(). Syscall6 differs from syscall.[Raw]Syscall6() insofar as
+// it is simultaneously executed on every pthread of the combined Go
+// and CGo runtimes.
 func Syscall6(syscallnr, arg1, arg2, arg3, arg4, arg5, arg6 uintptr) (uintptr, uintptr, syscall.Errno) {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
 	v := C.psx_syscall6(C.long(syscallnr), C.long(arg1), C.long(arg2), C.long(arg3), C.long(arg4), C.long(arg5), C.long(arg6))
 	var errno syscall.Errno
 	if v < 0 {
-		errno = syscall.Errno(C.__errno_too())
+		errno = syscall.Errno(C.__errno_too(-1))
 	}
 	return uintptr(v), uintptr(v), errno
 }
--- psx/psx_test.go
+++ psx/psx_test.go
@@ -1,6 +1,7 @@
 package psx
 
 import (
+	"runtime"
 	"syscall"
 	"testing"
 )
@@ -30,5 +31,36 @@ func TestSyscall6(t *testing.T) {
 		t.Errorf("malformed capget errno %d: %v (want 14: %v)", err, err, syscall.Errno(14))
 	} else if ^got != 0 {
 		t.Errorf("malformed capget did not return -1, got=%d", got)
+	}
+}
+
+// The man page for errno indicates that it is never set to zero, so
+// validate that it retains its value over a successful Syscall[36]()
+// and is overwritten on a failing syscall.
+func TestErrno(t *testing.T) {
+	// This testing is much easier if we don't have to guess which
+	// thread is running this Go code.
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	// Start from a known bad state and clean up afterwards.
+	setErrno(int(syscall.EPERM))
+	defer setErrno(0)
+
+	v3, _, errno := Syscall3(syscall.SYS_GETUID, 0, 0, 0)
+	if errno != 0 {
+		t.Fatalf("psx getuid failed: %v", errno)
+	}
+	v6, _, errno := Syscall6(syscall.SYS_GETUID, 0, 0, 0, 0, 0, 0)
+	if errno != 0 {
+		t.Fatalf("psx getuid failed: %v", errno)
+	}
+
+	if v3 != v6 {
+		t.Errorf("psx getuid failed to match v3=%d, v6=%d", v3, v6)
+	}
+
+	if v := setErrno(-1); v != int(syscall.EPERM) {
+		t.Errorf("psx changes prevailing errno got=%v(%d) want=%v", syscall.Errno(v), v, syscall.EPERM)
 	}
 }
