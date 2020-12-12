90192cd36471c7dfb44b4ecd6a8ccf7595d26e9d
Andrew G. Morgan <morgan@kernel.org>
2020-12-11 23:27:50 -0800

    Refactor the "psx" vs "cap" package cgo-or-not complexity to "psx".
    I've decided to put the decision to call syscall.AllThreadsSyscall*()
    into "psx" instead of the "cap" package. This should make client use of
    "psx" more straightforward and the code will 'just compile' if the
    user builds with CGO_ENABLED=1 or CGO_ENABLED=0. It also makes the "psx"
    package useful for other applications besides the "cap" package.
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- cap/syscalls.go
+++ cap/syscalls.go
@@ -1,14 +1,18 @@
-// +build linux,allthreadssyscall,!cgo
-
 package cap
 
-import "syscall"
+import (
+	"syscall"
+
+	"kernel.org/pub/linux/libs/security/libcap/psx"
+)
 
 // multisc provides syscalls overridable for testing purposes that
 // support a single kernel security state for all OS threads.
+// We use this version when we are cgo compiling because
+// we need to manage the native C pthreads too.
 var multisc = &syscaller{
-	w3: syscall.AllThreadsSyscall,
-	w6: syscall.AllThreadsSyscall6,
+	w3: psx.Syscall3,
+	w6: psx.Syscall6,
 	r3: syscall.RawSyscall,
 	r6: syscall.RawSyscall6,
 }
