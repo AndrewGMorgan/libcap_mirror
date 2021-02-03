a8392c96e5573251407c16006a437cc3ab705164
Andrew G. Morgan <morgan@kernel.org>
2021-02-02 19:09:08 -0800

    Add some CGO_ENABLED=0 documentation for the Syscall functions.
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- psx/psx.go
+++ psx/psx.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 )
 
+// Syscall3 and Syscall6 are aliases for syscall.AllThreadsSyscall*
+// when compiled CGO_ENABLED=0.
 var (
 	Syscall3 = syscall.AllThreadsSyscall
 	Syscall6 = syscall.AllThreadsSyscall6
