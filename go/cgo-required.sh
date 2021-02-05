068edb72653750c717aa6ea447b750157559325e
Andrew G. Morgan <morgan@kernel.org>
2021-02-04 21:13:27 -0800

    Make more uniform GO build options.
    Avoid building any of the GO stuff:
    
       make GOLANG=no ...
    
    Build with a specific build of GO:
    
       make GO=~/sdk/go1.16rc1/bin/go ...
    
    Also, now https://github.com/golang/go/issues/43149 is resolved in the
    go1.16rc1 build (it does not work in the go1.16beta1 but we don't support
    that one any more), remove the forced CGO use for the go/psx-signals build.
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- go/cgo-required.sh
+++ go/cgo-required.sh
@@ -4,8 +4,13 @@
 # available to the working go runtime or not. If it isn't we always
 # have to use libcap/psx to get POSIX semantics for syscalls that
 # change security state.
+if [ -n "$1" ]; then
+    export GO="${1}"
+else
+    export GO=go
+fi
 
-if [ -z "$(go doc syscall 2>/dev/null|grep AllThreadsSyscall)" ]; then
+if [ -z "$(${GO} doc syscall 2>/dev/null|grep AllThreadsSyscall)" ]; then
     echo "1"
 else
     echo "0"
