4f96e6788d535da5f57a3452a54b8d92bd41cd8e
Andrew G. Morgan <morgan@kernel.org>
2022-09-24 13:37:39 -0700

    Up the release version to 2.66
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- libcap/include/sys/capability.h
+++ libcap/include/sys/capability.h
@@ -18,7 +18,7 @@ extern "C" {
  * Provide a programmatic way to #ifdef around features.
  */
 #define LIBCAP_MAJOR 2
-#define LIBCAP_MINOR 65
+#define LIBCAP_MINOR 66
 
 /*
  * This file complements the kernel file by providing prototype
