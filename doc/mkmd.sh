27954dde34fcb8c306fa4d80fdbb5950f0949e03
Andrew G. Morgan <morgan@kernel.org>
2023-02-06 17:55:54 -0800

    Provide a method to import preamble and postscript for md man pages
    If you have local files:
    
       .../libcap/doc/local-md.preamble
       .../libcap/doc/local-md.postscript
    
    when you run .../libcap/doc/mkmd.sh these two files will be inlined
    into the generated index.md file.
    
    This addresses:
    
       https://bugzilla.kernel.org/show_bug.cgi?id=217007
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- doc/mkmd.sh
+++ doc/mkmd.sh
@@ -50,6 +50,14 @@ function do_page () {
 cat > "${index}" <<EOF
 # Manpages for libcap and libpsx
 
+EOF
+
+if [[ -f "local-md.preamble" ]]; then
+    cat "local-md.preamble" >> "${index}"
+fi
+
+cat >> "${index}" <<EOF
+
 ## Individual reference pages
 EOF
 
@@ -68,6 +76,14 @@ done
 cat >> "${index}" <<EOF
 
 ## More information
+
+EOF
+
+if [[ -f "local-md.postscript" ]]; then
+    cat "local-md.postscript" >> "${index}"
+fi
+
+cat >> "${index}" <<EOF
 
 For further information, see the
 [FullyCapable](https://sites.google.com/site/fullycapable/) homepage
