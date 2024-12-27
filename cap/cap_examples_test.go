2dda39e0e359134d49989c2549f5459ca89e4440
Andrew G. Morgan <morgan@kernel.org>
2024-12-26 19:26:54 -0800

    The go.dev site is unable to render this example.
    As a workaround for the golang bug: 70630, specifically comment
    70630#issuecomment-2513491664 provide an alias for the package.
    Perhaps, at some point, that bug or the one it is duped into will
    be fixed, and we can remove this workaround.
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- cap/cap_examples_test.go
+++ cap/cap_examples_test.go
@@ -6,18 +6,21 @@ import (
 	"fmt"
 	"log"
 
-	"kernel.org/pub/linux/libs/security/libcap/cap"
+	bug70630 "kernel.org/pub/linux/libs/security/libcap/cap"
 )
 
 func ExampleSet_Fill() {
-	c, err := cap.FromText("cap_setfcap=p")
+	// `bug70630` should just be `cap` (i.e there is no need for
+	// "real" code to alias the package), but that bug explains
+	// that the go.dev website is unable to support this.
+	c, err := bug70630.FromText("cap_setfcap=p")
 	if err != nil {
 		log.Fatalf("failed to parse: %v", err)
 	}
-	c.Fill(cap.Effective, cap.Permitted)
-	c.ClearFlag(cap.Permitted)
-	c.Fill(cap.Inheritable, cap.Effective)
-	c.ClearFlag(cap.Effective)
+	c.Fill(bug70630.Effective, bug70630.Permitted)
+	c.ClearFlag(bug70630.Permitted)
+	c.Fill(bug70630.Inheritable, bug70630.Effective)
+	c.ClearFlag(bug70630.Effective)
 	fmt.Println(c)
 	// Output: cap_setfcap=i
 }
