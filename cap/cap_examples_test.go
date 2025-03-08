824333d997f7f424c86f3622563494f2102c8376
Andrew G. Morgan <morgan@kernel.org>
2025-03-08 15:44:04 -0800

    Some more runnable documentation examples.
    All examples so far:
    
    https://pkg.go.dev/kernel.org/pub/linux/libs/security/libcap/cap@v1.2.76-rc3#pkg-examples
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

--- cap/cap_examples_test.go
+++ cap/cap_examples_test.go
@@ -5,6 +5,7 @@ package cap_test
 import (
 	"fmt"
 	"log"
+	"syscall"
 
 	cap_ "kernel.org/pub/linux/libs/security/libcap/cap"
 )
@@ -122,4 +123,74 @@ func ExampleImport() {
 	}
 	fmt.Printf("import of %02x is %q\n", d, c)
 	// Output: import of 90c2015101808000 is "cap_setuid=ep"
+}
+
+func ExampleSetUID() {
+	// This example does not need to use a package alias `cap_`.
+	// It is declared this way to make examples on the go.dev doc
+	// site render and run. This workaround is needed until the
+	// site is fixed as per:
+	// https://github.com/golang/go/issues/70611
+	c := cap_.GetProc()
+	if on, err := c.GetFlag(cap_.Permitted, cap_.SETUID); err != nil {
+		fmt.Printf("unable to determine cap_setuid permitted flag value: %v\n", err)
+		return
+	} else if !on {
+		fmt.Println("no permitted capability, try: sudo setcap cap_setuid=p program")
+		return
+	}
+	pre := syscall.Getuid()
+	const nobodyUID = 65534
+	if err := cap_.SetUID(nobodyUID); err != nil {
+		fmt.Printf("failed to set uid (%d): %v\n", nobodyUID, err)
+		return
+	}
+	post := syscall.Getuid()
+	fmt.Printf("UID was:%d, is now:%d (nobody=%v)\n", pre, post, post == nobodyUID)
+	c = cap_.GetProc()
+	fmt.Printf("all effective capabilities should be lowered: %q\n", c)
+}
+
+func ExampleFromText() {
+	// This example does not need to use a package alias `cap_`.
+	// It is declared this way to make examples on the go.dev doc
+	// site render and run. This workaround is needed until the
+	// site is fixed as per:
+	// https://github.com/golang/go/issues/70611
+	ts := []string{"=p all+ei", "all=pie", "=pi all+e", "=eip"}
+	expect, err := cap_.FromText("=ie all+p")
+	good := true
+	if err != nil {
+		fmt.Printf("failed to parse expected set: %v\n", err)
+		good = false
+	}
+	for _, t := range ts {
+		got, err := cap_.FromText(t)
+		if err != nil {
+			fmt.Printf("failed to parse %q: %v\n", t, err)
+			good = false
+		}
+		if cf, err := got.Cf(expect); err != nil || cf != 0 {
+			fmt.Printf("comparison failed for %q got %q (want: %q): %v\n", t, got, expect, err)
+			good = false
+		}
+	}
+	fmt.Printf("all examples passed=%v\n", good)
+	// Output: all examples passed=true
+}
+
+func ExampleFromName() {
+	// This example does not need to use a package alias `cap_`.
+	// It is declared this way to make examples on the go.dev doc
+	// site render and run. This workaround is needed until the
+	// site is fixed as per:
+	// https://github.com/golang/go/issues/70611
+	v := cap_.SYS_CHROOT
+	t, err := cap_.FromName(v.String())
+	if err != nil {
+		fmt.Printf("failed to parse %q: %v", v, err)
+		return
+	}
+	fmt.Printf("capability value %q of numerical value=%d correctly named\n", v, t)
+	// Output: capability value "cap_sys_chroot" of numerical value=18 correctly named
 }
